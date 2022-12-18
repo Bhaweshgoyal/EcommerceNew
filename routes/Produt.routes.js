89e38268b1d3d1c163b29c1060ea308f2bb1a431
Bhaweshgoyal <105268824+Bhaweshgoyal@users.noreply.github.com>
2022-12-18 21:04:41 +0530

    Ecom

---
+++ routes/Produt.routes.js
@@ -0,0 +1,17 @@
+const ProductItems = require("../controller/Product.controller");
+const { isAuthenticated, checkAdminOrSeller } = require("../Middlewares/Authentication.Validation");
+const { ValidateCreate, UpdateValidator, DeleteValidator} = require("../Middlewares/validators");
+const routes = (app)=>{
+    app.get("/ecomm/Product/HomePage",ProductItems.getMetoHomePage);
+    app.get("/ecomm/Product/getallProduct",ProductItems.getallProduct);
+    app.get("/ecomm/Product/getProduct/:productId" , ProductItems.getProductById)
+    app.get("/ecomm/Product/getallProduct/:categoryId",ProductItems.getallProductBycategoryId);
+    app.get("/ecomm/Product/getallProductByName/:name",ProductItems.getByName);
+    app.put("/ecomm/Product/between",ProductItems.getProductDataOfRange);
+    app.post("/ecomm/Product/CreateProductData",isAuthenticated,checkAdminOrSeller,ValidateCreate,ProductItems.CreateProduct);
+    app.put("/ecomm/Product/UpdateProductData",isAuthenticated,checkAdminOrSeller,UpdateValidator,ProductItems.UpdateProduct);
+    app.delete("/ecomm/Product/DeleteProduct",isAuthenticated,checkAdminOrSeller,DeleteValidator,ProductItems.DeleteProductContent);
+    
+}
+
+module.exports = routes;
